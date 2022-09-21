9f80fdc126d9686f33ecbe810e5de1f70cc1b231
mahid721 <bigdatamd2521@gmail.com>
2022-09-21 20:41:33 +0530

    renaming the file name

--- set_methods/set_methods_1.py
+++ set_methods/set_methods_1.py
@@ -5,7 +5,7 @@
 
 """
 add() :
-		Syntax -- set.add(elmnt)
+		syntax -- set_obj.add(elmnt)
 		functionality --  Adds an element to the set and If the element already exists, the add() method does not add the element.
 		args -- one mandatory argument -- add() takes single parameter(elem) which needs to  be added in the set.
 		returns -- The add() method doesn't return any value. --  None type
@@ -48,7 +48,7 @@ print(s3)
 #  -------------------------------------------- clear() -----------------------------------------
 """"
  clear()	 :	
-        syntax -- set.clear()
+        syntax -- set_obj.clear()
 		functionality --  Python Set clear() method removes all elements from the set.
 		args -- The clear() method for sets doesn’t take any parameters.
 		returns -- The clear() method doesn't return any value. --  None type
@@ -64,7 +64,7 @@ print(x)
 #  -------------------------------------------- update() -----------------------------------------
 """"
  update()	 :	
-        syntax -- set1.update(set2) 
+        syntax -- set_obj1.update(set2) 
 		functionality --  set adds elements from a set (passed as an argument) to the set.
 		args -- Update() method takes only a single argument. The single argument can be a set, list, tuples or a dictionary. It automatically converts into a set and adds to the set. 
 		returns -- The clear() method doesn't return any value. --  None type
@@ -79,7 +79,7 @@ print(s5)
 #  -------------------------------------------- union() -----------------------------------------
 """"
  union()	 :	
-        syntax -- set1.union(one/more sets) 
+        syntax -- set_obj1.union(one/more sets) 
 		functionality --  it merge to sets or 
 		args -- zero or more sets 
 		returns -- Returns a set, which has the union of all sets(set1, set2, set3…) with set1. It returns a copy of set1 only if no parameter is passed.
@@ -96,7 +96,7 @@ print(s10)
 #  -------------------------------------------- pop() -----------------------------------------
 """"
  pop()	 :	
-        syntax -- set.pop()
+        syntax -- set_obj.pop()
 		functionality --  Method removes any a random element from the set and returns the removed element.
 		args -- doesn’t take any parameter.
 		returns -- Returns the popped/removed element from the set
@@ -112,7 +112,7 @@ print(x)
 #  -------------------------------------------- remove() -----------------------------------------
 """"
  remove()	 :	
-        syntax -- set.remove(ele)
+        syntax -- set_obj.remove(ele)
 		functionality --  method removes the specified element from the set. 
 		                This method is different from the discard() method, 
 		                because the remove() method will raise an error if the specified item does not exist, and the discard() method will not.
@@ -130,7 +130,7 @@ print(s12)
 #  -------------------------------------------- discard() -----------------------------------------
 """"
  discard()	 :	
-        syntax -- set.discard(ele)
+        syntax -- set_obj.discard(ele)
 		functionality --  method removes the specified element from the set. 
 		                This method is different from the discard() method, 
 		                because the remove() method will raise an error if the specified item does not exist, and the discard() method will not.
